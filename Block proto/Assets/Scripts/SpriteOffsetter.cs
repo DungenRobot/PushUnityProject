990abbb33238424f7717cb247e77af67e48ebb71
NAishere <104450078+NAishere@users.noreply.github.com>
2022-06-27 14:28:26 -0400

    Sprite offset function
    Completed a function to set a gameObject to the nearest pixel relative to its parent

--- Block proto/Assets/Scripts/SpriteOffsetter.cs
+++ Block proto/Assets/Scripts/SpriteOffsetter.cs
@@ -11,8 +11,21 @@ public class SpriteOffsetter : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+
+        Debug.Log(RoundPixel(transform.parent.position) - transform.parent.position);
+
+        transform.localPosition = (RoundPixel(transform.parent.position) - transform.parent.position);
+    }
+
+    Vector3 RoundPixel(Vector3 input)
+    {
+        input *= 32;
+        input.x = Mathf.Round(input.x);
+        input.y = Mathf.Round(input.y);
+        input.z = Mathf.Round(input.z);
+        input /= 32;
+        return input;
     }
 }

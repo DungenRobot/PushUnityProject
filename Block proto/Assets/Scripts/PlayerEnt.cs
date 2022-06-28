cc8aba0765f01c15259039f4cd7552bc44979277
stefan <85657463+superstefan420@users.noreply.github.com>
2022-06-28 11:42:11 -0400

    Updated player control system

--- Block proto/Assets/Scripts/PlayerEnt.cs
+++ Block proto/Assets/Scripts/PlayerEnt.cs
@@ -6,7 +6,8 @@ public class PlayerEnt : MonoBehaviour
 {
 	private Vector3 oldDir = Vector3.down;
 	private Vector3 oldPos, newPos;
-	public float moveTime = 0.2f;
+	public float moveTime = 0.15f;
+	public float pushTime = 0.2f;
 	private bool isMoving = false;
 
     void Start()
@@ -33,19 +34,43 @@ public class PlayerEnt : MonoBehaviour
 	private IEnumerator 
 	Move(Vector3 dir) 
 	{
+		bool canmove = true;
+		bool pushing = false;
+		Vector3 aOldPos = Vector3.zero, aNewPos = Vector3.zero;
+
+		GameObject attached = null;
+		RaycastHit2D check = Physics2D.Raycast(transform.position + (Vector3.up * 0.5f), transform.TransformDirection(dir), 1f); 
+		Debug.DrawRay(transform.position + new Vector3(0.0f, 0.5f, 0.0f), transform.TransformDirection(dir), Color.green);
+		if (check) {
+			Collider2D c = check.collider;
+			Debug.Log(c);
+			if (c.gameObject.tag == "Block")
+				pushing = true;
+				attached = c.gameObject;
+				aOldPos = attached.transform.position;
+				aNewPos = aOldPos + dir;
+			if (c.gameObject.tag == "Obstacle")
+				canmove = false;
+		}
+		
 		isMoving = true;
 		
 		float elapsedTime = 0;
 
 		oldPos = transform.position;
 		newPos = oldPos + dir;
+		if (!canmove) newPos = oldPos;
 
-		while (elapsedTime < moveTime) {
+		while (elapsedTime < pushTime) {
+			if (pushing)
+				attached.transform.position = Vector3.Lerp(aOldPos, aNewPos, (elapsedTime / pushTime));
 			transform.position = Vector3.Lerp(oldPos, newPos, (elapsedTime / moveTime));
 			//transform.position = newPos;
 			elapsedTime += Time.deltaTime;
 			yield return null;
 		}
+		if (pushing)
+			attached.transform.position = aNewPos;
 		transform.position = newPos;
 		oldDir = dir;
 		isMoving = false;

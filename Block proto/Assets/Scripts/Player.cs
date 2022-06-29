8a4c3460bcd7b9a3c6e1caae852ca44fe1a3e64e
NAishere <104450078+NAishere@users.noreply.github.com>
2022-06-29 15:28:26 -0400

    Playercontroller v2 now fully functional

--- Block proto/Assets/Scripts/Player.cs
+++ Block proto/Assets/Scripts/Player.cs
@@ -13,18 +13,18 @@ Player : MonoBehaviour
 	private LevelExit exit;
 
 	private Vector3 oldDir = Vector3.down;
-	private Vector3 blockNewPos, newPos, oldPos;
+	private Vector3 blockNewPos, blockOldPos, newPos, oldPos;
 
     private GameObject block = null;
 
-    public float lerpTime;
+    private float lerpTime;
 
 	public float timeToMove = 0.01f;
 	public float pushTime = 100f;
 
+    private bool pushing = false;
+
     private float moveTime;
-	
-	private bool isMoving = false;
 
     void Start()
     {
@@ -51,9 +51,14 @@ Player : MonoBehaviour
 
         lerpTime += Time.deltaTime / moveTime;
 		
-        Debug.Log("--");
-        Debug.Log(oldPos);
-        Debug.Log(newPos);
+        //Debug.Log("--");
+        //Debug.Log(oldPos);
+        //Debug.Log(newPos);
+
+        if (pushing)
+        {
+            block.transform.position = Vector3.Lerp(blockOldPos, blockNewPos, lerpTime);
+        }
         transform.position = Vector3.Lerp(oldPos, newPos, lerpTime);
 
         // if ((transform.position - newPos).magnitude != 0)
@@ -64,7 +69,14 @@ Player : MonoBehaviour
 
 	void setMove(Vector3 moveDirection)
 	{
+        _audioSource.Stop();
         lerpTime = 0f;
+
+        if (pushing)
+        {
+            block.transform.position = blockNewPos;
+            block = null;
+        } 
 
         oldPos = newPos;
 		transform.position = newPos;
@@ -73,42 +85,45 @@ Player : MonoBehaviour
 
         moveTime = timeToMove;
 
-        //bool canmove = true;
-        //bool pushing = false;
+        pushing = false;
 
         //Raycast from where the player is to where the player is trying to move
-        //RaycastHit2D playerCast = Physics2D.Raycast(transform.position, newPos, 1f);
+        RaycastHit2D playerCast = Physics2D.Raycast(transform.position, moveDirection, 1f);
 
-        // if (playerCast.collider.gameObject != null) 
-        // {
-        //     if (playerCast.collider.gameObject.tag == "Block")
-        //     {
-        //         RaycastHit2D blockCast = Physics2D.Raycast(transform.position + moveDirection, newPos + moveDirection, 1f);
-        //         if (blockCast.collider == null || blockCast.collider.gameObject.tag == "Button")
-        //         {
-        //             //get the block for position changing later
-        //             pushing = true;
-		// 			block = playerCast.collider.gameObject;
+        if (playerCast.collider != null) 
+        {
+            Collider2D collision = playerCast.collider;
+            if (collision.gameObject.tag == "Block")
+            {
+                RaycastHit2D blockCast = Physics2D.Raycast(transform.position + moveDirection, moveDirection, 1f);
+                if (blockCast.collider == null || blockCast.collider.gameObject.tag == "Button")
+                {
                     
-		// 			blockNewPos = block.transform.position + moveDirection;
+                    pushing = true;
+                    moveTime = pushTime;
 
-        //             //play audio
-        //             _audioSource.clip = stone_move;
-		// 			_audioSource.Play();
-        //             return;
+					block = collision.gameObject;
 
-        //         }
-        //         else
-        //         {
-        //             newPos = transform.position;
-        //             return;
-        //         }
-        //     }
-        //     if (playerCast.collider.gameObject.tag == "Obstacle")
-        //     {
-        //         newPos = transform.position;
-        //         return;
-        //     }
-        // }
+                    blockOldPos = block.transform.position;
+					blockNewPos = block.transform.position + moveDirection;
+
+                    //play audio
+                    _audioSource.clip = stone_move;
+					_audioSource.Play();
+                    return;
+
+                }
+                else
+                {
+                    newPos = oldPos;
+                    return;
+                }
+            }
+            if (playerCast.collider.gameObject.tag == "Obstacle")
+            {
+                newPos = oldPos;
+                return;
+            }
+        }
 	}
 }
